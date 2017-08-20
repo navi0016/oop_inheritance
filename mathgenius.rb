5133482a7db2b88f1b389a7f36c112fb3eedf8e2
Navi Mann <navdeep.k.mann@gmail.com>
2017-08-20 18:29:44 -0400

    cleaned up mathgenius file

--- mathgenius.rb
+++ mathgenius.rb
@@ -2,8 +2,6 @@ require_relative 'multilinguist'
 
 class Mathgenius < Multilinguist
 
-  # @@list_of_numbers = []
-
   def initialize
     super
   end
@@ -15,9 +13,8 @@ class Mathgenius < Multilinguist
           sum += num
         end
       end
-    puts(self.say_in_local_language("The total is #{sum}."))
+    (self.say_in_local_language("The total is #{sum}."))
   end
-
 
 end
 
@@ -28,11 +25,3 @@ bob.travel_to("India")
 puts bob.report_total([1,2,3,4,5,6])
 bob.travel_to("Italy")
 puts bob.report_total([1,2,3,4,5,6])
-
-
-
-#world-travelling math genius - can speack many languages
-#and mentally add up huge lists of numbers
-
-#instances of this class should be able to accept a list of numbers
-# and return a sentence stating the sum of the numbers
